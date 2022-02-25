861497e7f7da09bfc8556875c7e97733c8ee8d5b
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-02-25 11:26:29 -0600

    added functions to get character type criteria and combine those with the length criterion into an object

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,7 +7,7 @@ var generateBtn = document.querySelector("#generate");
 var getPasswordLength = function() {
   var length = window.prompt("How long do you want your password to be? Enter a number of characters between 8 and 128.");
   length = parseInt(length);
-  if (length < 128 && length > 8) {
+  if (length <= 128 && length >= 8) {
     return length
   }
   else {
@@ -16,10 +16,39 @@ var getPasswordLength = function() {
   }
 };
 
-var generatePassword = function() {
-  var length = getPasswordLength();
-  console.log(length);
+var getCharCriteria = function() {
+  var includeLowercase = window.confirm("Do you want to include lowercase letters in your password?");
+  var includeUppercase = window.confirm("Do you want to include uppercase letters in your password?");
+  var includeNumeric = window.confirm("Do you want to include numbers in your password?");
+  var includeSpecial = window.confirm("Do you want to include special characters in your password?");
+
+  if (includeLowercase || includeUppercase || includeNumeric || includeSpecial) {
+    var charCritera = {
+      lowercase : includeLowercase,
+      uppercase : includeUppercase,
+      numeric : includeNumeric,
+      special : includeSpecial
+    };
+    return charCritera
+  }
+  else {
+    window.alert("You must choose at least one type of character.");
+    return getCharCriteria()
+  }
 }
+
+var getPasswordCriteria = function() {
+  var length = getPasswordLength();
+  var passwordCriteria = getCharCriteria();
+  passwordCriteria.length = length;
+  return passwordCriteria
+}
+
+var generatePassword = function() {
+  passwordCriteria = getPasswordCriteria();
+};
+
+generatePassword();
 
 // Write password to the #password input
 function writePassword() {
@@ -29,8 +58,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-generatePassword();
 
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
